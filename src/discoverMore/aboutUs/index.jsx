825f48e85d4d2c59a16dc7ee1c438feddfcd2456
faizan-al-ghurair <faizan.muzaffar@al-ghurair.com>
2023-09-28 10:56:17 +0400

    about us sections

---
+++ src/discoverMore/aboutUs/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../common/styles/headerStyles.css";
+import { Header } from "../../home/header";
+
+export const HeaderAboutUs = () => {
+  return (
+    <>
+      <Header></Header>
+      {/* <Sections1></Sections1> */}
+    </>
+  );
+};
