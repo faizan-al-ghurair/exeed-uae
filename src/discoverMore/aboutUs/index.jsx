3ebdbcd912bc1b2684e4cd547d46198c7596363d
faizan-al-ghurair <faizan.muzaffar@al-ghurair.com>
2023-10-04 09:43:51 +0400

    common style css added

--- src/discoverMore/aboutUs/index.jsx
+++ src/discoverMore/aboutUs/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../common/styles/headerStyles.css";
+import "../../common/styles/commonStyle.css";
 import { Header } from "../../home/header";
 
 export const HeaderAboutUs = () => {
