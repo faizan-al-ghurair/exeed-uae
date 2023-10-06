da825dbefd2fce5e0ac327033455e102a9864792
sarath <sarath.gangadharan@al-ghurair.com>
2023-10-06 11:21:23 +0530

    Showroom All Files

--- src/showRoom/abuDhabi/section1/index.js
+++ src/showRoom/abuDhabi/section1/index.js
@@ -9,9 +9,14 @@ export const Section1 = () => {
         data-element_type="section"
         data-settings="">
        
-
-       <h2 class="showroom-heading-title">COMING SOON</h2>
-
+       <div class="container-fluid">
+          <div class="row"> 
+            <div class="col-12">
+              <h2 class="showroom-heading-title">COMING SOON</h2>
+            </div>           
+          </div>
+       </div>
+        
         
       </section>
     </>
