8ce7323041ebb36d64ffd80cde089a140c80e4e3
sarath <sarath.gangadharan@al-ghurair.com>
2023-10-10 17:20:34 +0530

    Dubai Final Files

--- src/showRoom/dubai/section1/index.js
+++ src/showRoom/dubai/section1/index.js
@@ -4,31 +4,26 @@ import React from "react";
 export const Section1 = () => {
   return(
     <>
-    <section class="findshowroom-container">
-      <div class="container-fluid">
-        <div class="row"> 
-          <div class="col-12">
-               <h2 class="showroom-findinfo-title">FIND INFO FOR SHOWROOM</h2>
-               <h1 class="showroom-findinfo-description">Dubai Showroom</h1>
-          </div>           
-        </div>
-     </div>
-    </section> 
+   
 
     <section class="findshowroom-contactdetails">
-      <div class="container-fluid">
+      <div class="container-fluid p-0">
           <div class="row"> 
             <div class="col-12">
 
               <div class="contactdetails-text contt1">
+                <span class="contactIconHolder"><i class='fa fa-phone'></i></span>
                 <span class="">+971 4 3995661</span>
               </div>
+            
                 
               <div class="contactdetails-text contt2">
+              <span class="contactIconHolder"> <i class='fa fa-suitcase'></i></span>
                 <span class="">Weekday Timings : 9am to 8pm / Saturday: 9am to 8pm</span>
               </div>
 
               <div class="contactdetails-text contt3">
+              <span class="contactIconHolder"> <i class='fa fa-envelope'></i></span>
                 <span class="">exeed.UAE@al-ghurair.com</span>
               </div>
                
