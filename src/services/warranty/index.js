79326b3e870ede261434f87bd561fed349b8a7b9
KhadijaAMeslem <khadija.meslem@al-ghurair.com>
2023-10-12 11:06:05 +0400

    Fixed Home Carousel

--- src/services/warranty/index.js
+++ src/services/warranty/index.js
@@ -1,13 +1,4 @@
 import React from "react";
-import { Section1 } from "./section1";
-import { Header } from "../../home/header";
-// import { Section2 } from "./section2";
-// import { Section3 } from "./section3";
-// import { Section4 } from "./section4";
-// import { Section5 } from "./section5";
-// import { Section6 } from "./section6";
-import { Footer } from "../../home/section6";
-
 import { Header } from "../../home/header";
 import { Footer } from "../../home/section6";
 import { Section1 } from "./section1";
