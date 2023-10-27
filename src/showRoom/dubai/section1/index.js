ed9c09f6d1cfb690b32892442ec88a7ca8aac370
sarath <sarath.gangadharan@al-ghurair.com>
2023-10-27 18:25:06 +0530

    AcceptCookie_Page

--- src/showRoom/dubai/section1/index.js
+++ src/showRoom/dubai/section1/index.js
@@ -3,34 +3,32 @@ import React from "react";
 
 export const Section1 = () => {
   return(
-    <>
-   
+    <> 
+      <section class="findshowroom-contactdetails">
+        <div class="container-fluid p-0">
+            <div class="row"> 
+              <div class="col-12">
 
-    <section class="findshowroom-contactdetails">
-      <div class="container-fluid p-0">
-          <div class="row"> 
-            <div class="col-12">
+                <div class="contactdetails-text contt1">
+                  <span class="contactIconHolder"><i class='fa fa-phone'></i></span>
+                  <span class="">+971 4 3995661</span>
+                </div>
+              
+                  
+                <div class="contactdetails-text contt2">
+                <span class="contactIconHolder"> <i class='fa fa-suitcase'></i></span>
+                  <span class="">Weekday Timings : 9am to 8pm / Saturday: 9am to 8pm</span>
+                </div>
 
-              <div class="contactdetails-text contt1">
-                <span class="contactIconHolder"><i class='fa fa-phone'></i></span>
-                <span class="">+971 4 3995661</span>
-              </div>
-            
+                <div class="contactdetails-text contt3">
+                <span class="contactIconHolder"> <i class='fa fa-envelope'></i></span>
+                  <span class="">exeed.UAE@al-ghurair.com</span>
+                </div>
                 
-              <div class="contactdetails-text contt2">
-              <span class="contactIconHolder"> <i class='fa fa-suitcase'></i></span>
-                <span class="">Weekday Timings : 9am to 8pm / Saturday: 9am to 8pm</span>
-              </div>
-
-              <div class="contactdetails-text contt3">
-              <span class="contactIconHolder"> <i class='fa fa-envelope'></i></span>
-                <span class="">exeed.UAE@al-ghurair.com</span>
-              </div>
-               
-            </div>           
-          </div>
-      </div>
-    </section>
+              </div>           
+            </div>
+        </div>
+      </section>
     </>   
   );
  
